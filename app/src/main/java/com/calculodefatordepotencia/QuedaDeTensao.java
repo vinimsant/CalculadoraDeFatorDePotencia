53724c61cdb6ba548bee3baea5d431f9f4e48a27
vinimsant <vini.v2@hotmail.com>
2021-11-21 20:21:54 -0300

    start

---
+++ app/src/main/java/com/calculodefatordepotencia/QuedaDeTensao.java
@@ -0,0 +1,14 @@
+package com.calculodefatordepotencia;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class QuedaDeTensao extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_queda_de_tensao);
+    }
+}
