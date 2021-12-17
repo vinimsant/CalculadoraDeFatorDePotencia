9adeed2df912a027aee96ca3f157052bd817a338
vinimsant <vini.v2@hotmail.com>
2021-12-17 18:45:46 -0300

    começo da tela queda de tensao fragment

--- app/src/main/java/com/calculodefatordepotencia/activity/activity/QuedaDeTensao.java
+++ app/src/main/java/com/calculodefatordepotencia/activity/activity/QuedaDeTensao.java
@@ -1,17 +1,43 @@
 package com.calculodefatordepotencia.activity.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.transition.FragmentTransitionSupport;
 
+import android.app.FragmentTransaction;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.FrameLayout;
 
 import com.calculodefatordepotencia.R;
+import com.calculodefatordepotencia.activity.fragmenty.MonoFragment;
+import com.calculodefatordepotencia.activity.fragmenty.TrifasicoFragment;
 
 public class QuedaDeTensao extends AppCompatActivity {
+
+    private Button btnMono;
+    private Button btnTrifasico;
+    private MonoFragment monoFragment;
+    private TrifasicoFragment trifasicoFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_queda_de_tensao);
+
+        //configurando botôes.
+        btnMono = findViewById(R.id.btnMono);
+        btnTrifasico = findViewById(R.id.btnTrifasico);
+
+        //configurando classes
+        monoFragment = new MonoFragment();
+        trifasicoFragment = new TrifasicoFragment();
+
+        //configurar o objeto para ser exibido no frame layot
+        /*FragmentTransaction transaction = getFragmentManager().beginTransaction();
+        transaction.replace(R.id.frameLayoutQuedaDeTensao, monoFragment);
+        transaction.commit();*/
+
+
     }
 
 }
---
+++ app/src/main/java/com/calculodefatordepotencia/activity/fragmenty/MonoFragment.java
@@ -0,0 +1,66 @@
+package com.calculodefatordepotencia.activity.fragmenty;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.calculodefatordepotencia.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link MonoFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class MonoFragment extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public MonoFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment MonoFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static MonoFragment newInstance(String param1, String param2) {
+        MonoFragment fragment = new MonoFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_mono, container, false);
+    }
+}
