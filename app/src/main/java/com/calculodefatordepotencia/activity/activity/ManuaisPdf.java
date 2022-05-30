01789fd41ff3fc7c88dd397f96dbf571a2b58775
vinimsant <vini.v2@hotmail.com>
2022-05-30 17:37:49 -0300

    substituir a activity manuais por manuais1_0 e adicionei uma activite para cada marca de inversor

--- app/src/main/java/com/calculodefatordepotencia/activity/activity/ManuaisPdf.java
+++ app/src/main/java/com/calculodefatordepotencia/activity/activity/ManuaisPdf.java
@@ -5,12 +5,19 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.calculodefatordepotencia.R;
+import com.github.barteksc.pdfviewer.PDFView;
 
 public class ManuaisPdf extends AppCompatActivity {
+
+    PDFView pdfView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_manuais_pdf);
+
+        // recuperando o pdfviewr
+        pdfView = (PDFView) findViewById(R.id.pdfview);
+        pdfView.fromAsset("Manual-do-Usuario-CFW08.pdf").load();
     }
 }
--- app/src/main/java/com/calculodefatordepotencia/activity/activity/inversores/InversoresWeg.java
+++ app/src/main/java/com/calculodefatordepotencia/activity/activity/inversores/InversoresWeg.java
@@ -2,15 +2,29 @@ package com.calculodefatordepotencia.activity.activity.inversores;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import com.calculodefatordepotencia.R;
+import com.calculodefatordepotencia.activity.activity.ManuaisPdf;
 
 public class InversoresWeg extends AppCompatActivity {
+
+    ImageView cfw08;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inversores_weg);
+        cfw08 = findViewById(R.id.cfw08);
+        cfw08.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), ManuaisPdf.class);
+                startActivity(intent);
+            }
+        });
     }
 }
