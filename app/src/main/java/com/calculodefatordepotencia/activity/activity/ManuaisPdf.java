4df86ec1606706f7fcabcae6afebb1f0e748952d
vinimsant <vini.v2@hotmail.com>
2022-06-01 18:03:38 -0300

    substituir a activity manuais por manuais1_0 e adicionei uma activite para cada marca de inversor

--- app/src/main/java/com/calculodefatordepotencia/activity/activity/ManuaisPdf.java
+++ app/src/main/java/com/calculodefatordepotencia/activity/activity/ManuaisPdf.java
@@ -1,15 +1,19 @@
 package com.calculodefatordepotencia.activity.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 
 import com.calculodefatordepotencia.R;
 import com.github.barteksc.pdfviewer.PDFView;
+import com.github.barteksc.pdfviewer.listener.OnPageChangeListener;
+import com.github.barteksc.pdfviewer.scroll.DefaultScrollHandle;
 
 public class ManuaisPdf extends AppCompatActivity {
 
     PDFView pdfView;
+    int page;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,7 +21,10 @@ public class ManuaisPdf extends AppCompatActivity {
         setContentView(R.layout.activity_manuais_pdf);
 
         // recuperando o pdfviewr
+        page = 0;
         pdfView = (PDFView) findViewById(R.id.pdfview);
-        pdfView.fromAsset("Manual-do-Usuario-CFW08.pdf").load();
+        pdfView.fromAsset("Manual-do-Usuario-CFW08.pdf")
+                .scrollHandle(new DefaultScrollHandle(this)).defaultPage(page).load();
+
     }
 }
